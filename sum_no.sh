701801646cfb11e1e82c6f169c9831a8977e89bb
send2kripa <send2kripa@gmail.com>
2018-12-10 19:47:10 +0530

    Again updated

--- sum_no.sh
+++ sum_no.sh
@@ -2,6 +2,6 @@
 
 echo 'This script is from Git Hub'
 
-sumNo = $(( $num1 + $num2 ))
-echo 'Sum of 2 numbers is' $sumNo
+sumNo=$(( $num1 + $num2 ))
+echo 'Sum of 2 numbers is:' $sumNo
 echo 'Script execution completed.'
