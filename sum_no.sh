aad9d5ee6255b4864f5e735dd57a4550ac6f2937
send2kripa <send2kripa@gmail.com>
2019-03-04 05:18:30 +0530

    Delete sum_no.sh

--- sum_no.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'This script is from Git Hub'
-
-sumNo=$(( $num1 + $num2 ))
-echo 'Sum of 2 numbers is:' $sumNo
-echo 'Script execution completed.'
-echo 'Adding aditional output'
